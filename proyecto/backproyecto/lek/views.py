0413b7a980667afaefab5b35b7b8aac5607ecc47
Jose Escobar <duocuc>
2022-06-04 19:53:02 -0400

    plantilla componentes

--- proyecto/backproyecto/lek/views.py
+++ proyecto/backproyecto/lek/views.py
@@ -9,6 +9,8 @@ def contactos (request):
     return render (request, 'lek/contactos.html')
     
 def componentes (request):
-    contexto = {"nombreC" :"Audifonos","nombreC1":"Hyperx Kraken" , "nombreC2":"Logitech G733", "nombreC3": "Hyperx Cloud Alpha","nombreC4" :"Hyperx Stinger Wireless","nombreC5": "Onikuma Rosado"}
-    return render (request, 'lek/componentes.html')
+    contexto = {"nombreC" :"Audifonos","nombreC1":"Hyperx Kraken" , "nombreC2":"Logitech G733", "nombreC3": "Hyperx Cloud Alpha","nombreC4" :"Hyperx Stinger Wireless","nombreC5": "Onikuma Rosado",
+                  "fotocomponente1" : "/static/lek/img/componentes/audifono1.png","fotocomponente2" : "/static/lek/img/componentes/audifono2.png" , "fotocomponente3" : "/static/lek/img/componentes/audifono3.png",
+                  "fotocomponente4" : "/static/lek/img/componentes/audifono4.png", "fotocomponente5" : "/static/lek/img/componentes/audifono5.png"}
+    return render (request, 'lek/componentes.html',contexto )
 
