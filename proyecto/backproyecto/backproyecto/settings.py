8d6861fe9e619c1522d605fe615f42e27e622396
Eleksinho <f.alvarezm@duocuc.cl>
2022-06-04 16:11:44 -0400

    aa

--- proyecto/backproyecto/backproyecto/settings.py
+++ proyecto/backproyecto/backproyecto/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'backproyecto.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'backproyecto.db',
     }
 }
 
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-pe'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
--- proyecto/backproyecto/lek/models.py
+++ proyecto/backproyecto/lek/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Curso(models.model):
+    codigo=models.CharField(primary_key=True, max_length=6)
+    nombre= models.CharField(max_length=50)
+    creditos= models.PositiveSmallIntegerField()
+
+    
