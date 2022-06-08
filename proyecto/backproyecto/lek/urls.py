58f19954b1500c95467c6e412165088ff846084d
Jose Escobar <duocuc>
2022-06-07 22:41:54 -0400

    Revert "a"
    This reverts commit d0b86760fff20fa8d8ccbe211ed7cd16bcad24b9, reversing
    changes made to edafffddeb6a84202d8af084db6dcdcbeb5ac6a9.

--- proyecto/backproyecto/lek/urls.py
+++ proyecto/backproyecto/lek/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import menu, componentes, contactos, nosotros,registro, login
+from .views import menu, componentes, contactos, nosotros,registro, login,carrito
 
 urlpatterns = [
     path('',menu,name='menu'),
-    path('componentes',componentes,name='componentes'),
+    path('componentes/',componentes,name='componentes'),
     path('contactos/',contactos,name='contactos'),
     path('nosotros/',nosotros,name='nosotros'),
     path('registro/',registro,name='registro'),
     path('login/',login,name='login'),
+    path('carrito/',carrito,name='carrito'),
 
     
 ]
